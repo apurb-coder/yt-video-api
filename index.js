700ec450374c494499f8dfcbda3809d81f67d791
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2023-12-16 15:22:25 +0530

    Create "Downloads" Folder when Server Starts

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import route from './Routes/routes.js' // last me .js extension is imp nahi toh error milega
 import cors from 'cors'
 import bodyParser from "body-parser";
+import fs from 'fs'
 
 const app=express();
 
@@ -12,6 +13,20 @@ app.use(cors()); //use it before backend routing
 app.use('/',route);
 
 const PORT= process.env.PORT || 8000;
+
+if(!fs.existsSync('./Downloads')){
+    try{
+        fs.mkdirSync('./Downloads');
+        console.log('Directory created');
+    }
+    catch(err){
+        console.log('Failed to create directory');
+        console.log(err);
+    }
+}
+else{
+    console.log('Already exists');
+}
 
 app.listen(PORT,()=>{
     console.log(`Server Running on Port-${PORT}`)
